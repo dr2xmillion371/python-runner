6177823d09528da619c1b872173d94dc44dcc945
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-07 08:52:35 -0500

    shell

---
+++ shell.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------
+# THE PYTHON NEW SHELL (only executes one line of code)
+#-------------------------------------------------------
+
+
+
+def shell():
+  users_code = input('dshell > ')
+  exec(users_code)
+  
+
+
+
+
+
+
+shell()
